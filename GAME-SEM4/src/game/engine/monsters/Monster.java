201ef5ef4ebbf3e3c5cf5c9c40eea25da26a84f2
omarahmed07205-collab <omar.ahmed07205@gmail.com>
2026-02-27 23:30:54 +0200

    5.4,5.5,5.10,5.11,5.12

--- GAME-SEM4/src/game/engine/monsters/Monster.java
+++ GAME-SEM4/src/game/engine/monsters/Monster.java
@@ -1,109 +1,109 @@
 package game.engine.monsters;
+
 import game.engine.Role;
 
-public abstract class Monster implements Comparable {
+public abstract class Monster implements Comparable<Monster> {
 
-	    // read only
-	    private final String name;
-	    private final String description;
-	    private final Role originalRole;
+    // read only
+    private final String name;
+    private final String description;
+    private final Role originalRole;
 
-	    // read,write
-	    private Role role;
-	    private int energy;            // >= 0
-	    private int position;          // 0 - 99
-	    private boolean frozen;
-	    private boolean shielded;
-	    private int confusionTurns;  
+    // read, write
+    private Role role;
+    private int energy;            // >= 0
+    private int position;          // 0 - 99
+    private boolean frozen;
+    private boolean shielded;
+    private int confusionTurns;
 
-	    public Monster(String name, String description, Role originalRole, int energy) {
-	        this.name = name;
-	        this.description = description;
-	        this.originalRole = originalRole;
-	        this.role = originalRole;
-	        setEnergy(energy); 
-	        
-	        this.position = 0;	        
-	        this.frozen = false;
-	        this.shielded = false;
-	        this.confusionTurns = 0;
-	    }
-	    
-	    public int compareTo(Monster o) {
-	        return this.position - o.position;
-	    }
-	    
-	    public String getName() {
-	        return name;
-	    }
+    public Monster(String name, String description, Role originalRole, int energy) {
+        this.name = name;
+        this.description = description;
+        this.originalRole = originalRole;
+        this.role = originalRole;
+        setEnergy(energy);
 
-	    public String getDescription() {
-	        return description;
-	    }
+        this.position = 0;
+        this.frozen = false;
+        this.shielded = false;
+        this.confusionTurns = 0;
+    }
 
-	    public Role getOriginalRole() {
-	        return originalRole;
-	    }
+    @Override
+    public int compareTo(Monster o) {
+        return this.position - o.position;
+    }
 
-	     // read , write
+    public String getName() {
+        return name;
+    }
 
-	    public Role getRole() {
-	        return role;
-	    }
+    public String getDescription() {
+        return description;
+    }
 
-	    public void setRole(Role role) {
-	        this.role = role;
-	    }
+    public Role getOriginalRole() {
+        return originalRole;
+    }
 
-	    public int getEnergy() {
-	        return energy;
-	    }
+    // read , write
+    public Role getRole() {
+        return role;
+    }
 
-	    public void setEnergy(int energy) {
-	        if (energy < 0)
-	            this.energy = 0;
-	        else
-	            this.energy = energy;
-	    }
+    public void setRole(Role role) {
+        this.role = role;
+    }
 
-	    public int getPosition() {
-	        return position;
-	    }
-        
-	   // to be edited 
-	    public void setPosition(int position) {
-	        if (position < 0)
-	            this.position = 0;
-	        else if (position > 99)
-	            this.position = position % 100;
-	        else
-	            this.position = position;
-	    }
+    public int getEnergy() {
+        return energy;
+    }
 
-	    public boolean isFrozen() {
-	        return frozen;
-	    }
+    public void setEnergy(int energy) {
+        if (energy < 0)
+            this.energy = 0;
+        else
+            this.energy = energy;
+    }
 
-	    public void setFrozen(boolean frozen) {
-	        this.frozen = frozen;
-	    }
+    public int getPosition() {
+        return position;
+    }
 
-	    public boolean isShielded() {
-	        return shielded;
-	    }
+    public void setPosition(int position) {
+        if (position < 0)
+            this.position = 0;
+        else if (position > 99)
+            this.position = position % 100;
+        else
+            this.position = position;
+    }
 
-	    public void setShielded(boolean shielded) {
-	        this.shielded = shielded;
-	    }
+    public boolean isFrozen() {
+        return frozen;
+    }
 
-	    public int getConfusionTurns() {
-	        return confusionTurns;
-	    }
+    public void setFrozen(boolean frozen) {
+        this.frozen = frozen;
+    }
 
-	    public void setConfusionTurns(int confusionTurns) {
-	        if (confusionTurns < 0)
-	            this.confusionTurns = 0;
-	        else
-	            this.confusionTurns = confusionTurns;
-	    }
+    public boolean isShielded() {
+        return shielded;
+    }
+
+    public void setShielded(boolean shielded) {
+        this.shielded = shielded;
+    }
+
+    public int getConfusionTurns() {
+        return confusionTurns;
+    }
+
+    public void setConfusionTurns(int confusionTurns) {
+        if (confusionTurns < 0)
+            this.confusionTurns = 0;
+        else
+            this.confusionTurns = confusionTurns;
+    }
 }
---
+++ GAME-SEM4/src/game/engine/monsters/Multitasker.java
@@ -0,0 +1,22 @@
+package game.engine.monsters;
+import game.engine.Role;
+
+public class Multitasker extends Monster {
+	
+	// READ & WRITE
+    private int normalSpeedTurns;
+    
+    public Multitasker(String name, String description, Role role, int energy) {
+        super(name, description, role, energy);
+        this.normalSpeedTurns = 0;
+    }
+    
+    public int getNormalSpeedTurns() {
+        return normalSpeedTurns;
+    }
+
+    public void setNormalSpeedTurns(int normalSpeedTurns) {
+        this.normalSpeedTurns = normalSpeedTurns;
+    }
+        
+}
