db348506b280d7d8cf1e650ea73a0ca15fca941c
Legend-T7 <abdelrahman310107@gmail.com>
2026-03-03 17:16:30 +0200

    csv files attached , 5.21 (Board class)
    originalCards attribute to be possibly edited later for the "final" keyword , if it's a must then implementation needs change

---
+++ GAME-SEM4/src/game/engine/Board.java
@@ -0,0 +1,51 @@
+package game.engine;
+
+import game.engine.monsters.Monster;
+import game.engine.cells.Cell;
+import game.engine.cards.Card; 
+import java.util.ArrayList;  // 34an el arraylist
+
+public class Board {
+	
+	 private final Cell [][] boardCells ;
+	 private static ArrayList<Monster> stationedMonsters;  // read w write 
+	 private static ArrayList<Card> originalCards ;// can't do final to be able to assign in constructor
+	 private static ArrayList<Card> cards ;                // read w write brdo
+	 
+	 public Board(ArrayList<Card> readCards){
+		 boardCells = new Cell[Constants.BOARD_ROWS][Constants.BOARD_COLS];
+		 stationedMonsters = new ArrayList<>();
+		 cards =  new ArrayList<>();
+		 originalCards = readCards ;
+	 }
+	 
+	  public Cell[][] getBoardCells() {
+	        return boardCells;
+	  }
+	  
+	  //////////////////////////////////
+	  
+	  public static ArrayList<Monster> getStationedMonsters() {
+	        return stationedMonsters;
+	  }
+
+	  public static void setStationedMonsters(ArrayList<Monster> monsters) {
+	        stationedMonsters = monsters;
+	  }
+
+	  ////////////////////////////////
+	  
+	  public static ArrayList<Card> getOriginalCards() {
+	        return originalCards;
+	  }
+
+	  ///////////////////////////////
+	  
+	  public static ArrayList<Card> getCards() {
+	        return cards;
+	  }
+
+	   public static void setCards(ArrayList<Card> newCards) {
+	        cards = newCards;
+	   }
+}
