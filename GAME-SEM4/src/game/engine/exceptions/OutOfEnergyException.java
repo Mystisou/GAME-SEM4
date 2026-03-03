223b8a51d79bc14e77917a8193ad8fbead1b5198
Hemo06 <hemoadel06@gmail.com>
2026-03-03 19:05:25 +0200

    Update OutOfEnergyException.java

--- GAME-SEM4/src/game/engine/exceptions/OutOfEnergyException.java
+++ GAME-SEM4/src/game/engine/exceptions/OutOfEnergyException.java
@@ -1,19 +1,12 @@
 package game.engine.exceptions;
 
-public class OutOfEnergyException {
-
-    static final String MSG = "Not Enough Energy for Power Up";
 public class OutOfEnergyException extends GameActionException{
     public static final String MSG = "Not Enough Energy for Power Up";
     
-    OutOfEnergyException(){
     public OutOfEnergyException(){
         super(MSG);
     }
-    
-    OutOfEnergyException(String message){
     public OutOfEnergyException(String message){
          super(message);
     }
-}}
 }
