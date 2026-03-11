9398d60f753f16cfb12e716766413b6d61e91e93
Mystisou <sondoselsamny1973@gmail.com>
2026-03-12 00:43:38 +0200

    Updated ContaminationSock
    Missing interface implementation and method modifyEnergy

--- GAME-SEM4/src/game/engine/cells/ContaminationSock.java
+++ GAME-SEM4/src/game/engine/cells/ContaminationSock.java
@@ -1,9 +1,15 @@
 package game.engine.cells;
+import game.engine.interfaces.CanisterModifier;
+import game.engine.monsters.Monster;
 
-public class ContaminationSock extends TransportCell {
+public class ContaminationSock extends TransportCell implements CanisterModifier {
 
     public ContaminationSock(String name, int effect) {
         super(name, -Math.abs(effect)); 
         //effect is always negative
+    }
+    
+    public void modifyEnergy(Monster monster) {
+    	
     }
 }
