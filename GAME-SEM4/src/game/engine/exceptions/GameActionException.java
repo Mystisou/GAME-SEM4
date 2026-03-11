eea1b74920f987fe688621ab92283d7234648c5b
Mystisou <sondoselsamny1973@gmail.com>
2026-03-12 01:27:47 +0200

    added space

--- GAME-SEM4/src/game/engine/exceptions/GameActionException.java
+++ GAME-SEM4/src/game/engine/exceptions/GameActionException.java
@@ -1,6 +1,6 @@
 package game.engine.exceptions;
 
-public abstract class GameActionException extends Exception{
+public abstract class GameActionException extends Exception {
 	
 	public GameActionException() {
 		super();
